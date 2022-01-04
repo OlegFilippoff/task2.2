63b958a960954249b9f51a4d8d00218eed88e6a2
Oleg Filippov <5157175@bk.ru>
2022-01-04 10:20:11 +0300

    homework 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-        {
-            int popUpSum = 100;
-            int popUpBonusSum;
-            popUpBonusSum = popUpSum / 100;
-            int currentBalance = 100;
-            int totalBalance;
-            if (popUpSum >= 1000) {
-                totalBalance = (currentBalance + popUpSum + popUpBonusSum);
-                System.out.println(totalBalance + " рублей на счете");
-                System.out.println(popUpBonusSum + " сумма бонусных рублей");
-            } else {
-                totalBalance = (currentBalance + popUpSum);
-                System.out.println(totalBalance + " Ваш баланс в рублях");
-            }
+
+        int topUpSum = 1100;
+        int topUpBonusSum;
+        topUpBonusSum = topUpSum / 100;
+        int currentBalance = 100;
+        int totalBalance;
+        if (topUpSum > 1000) {
+            totalBalance = (currentBalance + topUpSum + topUpBonusSum);
+            System.out.println(totalBalance + " рублей на счете");
+            System.out.println(topUpBonusSum + " сумма бонусных рублей");
+        } else {
+            totalBalance = (currentBalance + topUpSum);
+            System.out.println(totalBalance + " Ваш баланс в рублях");
         }
     }
 }
+
 
